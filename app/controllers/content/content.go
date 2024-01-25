85c070c78a68710e59564c17fb9773eddd691fbc
jjamieson1 <jjamie@gmail.com>
2024-01-24 19:06:41 -0700

    Changed to sprintf to format html template

--- app/controllers/content/content.go
+++ app/controllers/content/content.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+
 	content "github.com/jjamieson1/eden-sdk/clients/cms"
 	"github.com/jjamieson1/eden-sdk/models"
 	"github.com/revel/revel"
@@ -15,21 +17,37 @@ func (c Content) GetContentByCategoryId(pageName string) revel.Result {
 	revel.AppLog.Debugf("requesting content from categoryId: %s", categoryId)
 	var provider models.TenantProvider
 	provider.EdenAdapter.AdapterUrl = "http://localhost:3001/api/v1/content"
-	result := `<div>`
+	result := ""
+	image := ""
 	ctn, err := content.GetCmsFromProviderByCategoryId("", "", categoryId, provider)
 	if err != nil {
 		revel.AppLog.Errorf("unable to get content with error: %s", err.Error())
 	}
 	for _, a := range ctn {
-		result = result + `<div class="m-5">` + a.CmsContent.Body
 		if len(a.CmsContent.Images) > 0 {
 			for _, img := range a.CmsContent.Images {
-				result = result + `<div class="float-right"><img src="` + img.Filename + `/></div>`
+				image = img.Filename
 			}
 		}
-		result = result + `</div>`
+		result = fmt.Sprintf(`
+			<section class="py-12">
+				<div class="container">
+					<div class="row align-items-center justify-content-between">
+						<div class="col-12 col-md-6 col-lg-5">
+						<h2 class="mb-7">%s</h2> 
+						%s
+						</div>
+						<div class="col-12 col-md-6">
+							<div>
+								<img src="%s" alt="..." class="img-fluid w-100">
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			`, a.CmsContent.Title, a.CmsContent.Body, image)
+
 	}
-	result = result + `</div>`
 
 	return c.RenderHTML(result)
 }
--- app/controllers/menu/navigation.go
+++ app/controllers/menu/navigation.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+
 	content "github.com/jjamieson1/eden-sdk/clients/cms"
 	"github.com/jjamieson1/eden-sdk/models"
 	"github.com/revel/revel"
@@ -20,58 +22,29 @@ func (c Menu) GetMenu(pageName string) revel.Result {
 	if err != nil {
 		revel.AppLog.Errorf("unable to get content with error: %s", err.Error())
 	}
-	ctg := `<select name="categoryId" hx-target="#main" hx-trigger="change" hx-post="/components/content" hx-indicator=".htmx-indicator" class="form-select"><option selected>Pages</option>`
+	ctg := `<select name="categoryId" hx-target="#main" hx-post="/components/content" hx-indicator=".htmx-indicator" class="form-select"><option>Select Page</option>`
 	for _, cat := range contents {
 		ctg = ctg + `<option value="`
 		ctg = ctg + cat.CmsCategoryId + `">` + cat.Name + `</option>`
 	}
 	ctg = ctg + `</select>`
-	result := `
-	<nav class="navbar navbar-expand-lg navbar-light">
-	<div class="container-fluid">
-		<a class="navbar-brand" href="/">Celestial Technologies<span style="color:red">.</span></a>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarScroll">
-			<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-				
-				<li class="nav-item">
-					<a class="nav-link" aria-current="Home" href="/"`
-	if pageName == "home" {
-		result = result +
-			`
-		style="color:red;"
-		`
-	}
-
-	result = result +
-		`
-	>Home</a>
-	</li>
-	<li class="nav-item active">
-		<a class="nav-link" aria-current="Home" href="/pages/blog"
-	`
-
-	if pageName == "blog" {
-		result = result + `
-	style="color:red;"
-	`
-	}
-
-	result = result + `
-					>Blog</a>
-				</li>
-				<li class="nav-item">
-	`
-
-	result = result + ctg
-	result = result + `
-				</li>
-			</ul>
-		</div>
-	</div>
-</nav>
-`
+	result := fmt.Sprintf(`
+			<nav class="navbar navbar-expand-lg navbar-light">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="/">Celestial Technologies<span style="color:red">.</span></a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarScroll">
+					<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+						
+						<li class="nav-item">
+						%s
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		`, ctg)
 	return c.RenderHTML(result)
 }
